557b1852261c87823dbcaad0387927abc5ac79fb
jei16 <jhoncarlodc41603@gmail.com>
2023-09-04 11:22:11 -0700

    ongoing

--- receipt plant.py
+++ receipt plant.py
@@ -52,9 +52,17 @@ print(brdr1.format(a="", b="", c="", d="", e="", f="", g="", h="", i="", j="", k
 
 #
 brdr2 = "{a:│^1}{b:^28}{c:│^1}{d:^6}{e:│^1}{f:^6}{g:│^1}{h:^6}{i:│^1}{j:^6}{k:│^1}"
-for b in a:
-    print(
-        brdr2.format(
+print(
+    brdr2.format(
+            a="", b=b[0], c="", d=b[1], e="", f=b[2], g="", h=b[3], i="", j=b[4], k=""
+        )
+    )
+
+
+#
+brdr2p1 = "{a:│^1}{b:^28}{c:│^1}{d:^6}{e:│^1}{f:^6}{g:│^1}{h:^6}{i:│^1}{j:^6}{k:│^1}"
+print(
+    brdr2p1.format(
             a="", b=b[0], c="", d=b[1], e="", f=b[2], g="", h=b[3], i="", j=b[4], k=""
         )
     )
